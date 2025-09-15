038b1bc48d15754c73b3fc66d73180c55e339593
yusuf9900 <youssuf9900@gmail.com>
2025-09-15 09:55:56 +0000

    résolution du probléme de la page Cors pour que le backend fonction en prod

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,9 +6,9 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
+        env('FRONTEND_URL', 'http://localhost:5173', 'https://red-product-bakeli.netlify.app/'),
         'https://red-product-frontend.onrender.com',
-        'http://localhost:5173',
+        'http://localhost:5173', 'https://red-product-bakeli.netlify.app/'
     ],
 
     'allowed_origins_patterns' => [],
