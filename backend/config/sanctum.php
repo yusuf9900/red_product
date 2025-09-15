313069eef2b1a2aa61f3af07f1d7ec21b465e912
yusuf9900 <youssuf9900@gmail.com>
2025-09-15 02:31:09 +0000

    chore: update auth and sanctum configuration for production

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -16,10 +16,10 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+        '%s%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,',
+        'red-product-frontend.onrender.com,red-product-backend-brl6.onrender.com,',
+        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
